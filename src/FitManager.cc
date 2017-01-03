6be6e2e9b30eec8575746f090169b7e152fc6b55
Коваленко Алексей Витальевич <petrov.knu@gmail.com>
2017-01-03 18:39:00 +0200

    Wrong Mnimum Simplex

--- src/FitManager.cc
+++ src/FitManager.cc
@@ -326,10 +326,10 @@ void FitManager::PerformMinimization(const char * outputfile)
 	gMinimizer->mnexcm("SET PRI", arglist + 1,  1, ierflag);
 
 	ierflag = 0;
-	arglist[0] = 3000;
-	arglist[1] = 0.01;
-	gMinimizer->mnexcm("MIGRAD", arglist, 2, ierflag);
-	// gMinimizer->mnexcm("SIMPLEX", arglist, 2, ierflag);
+	arglist[0] = 5000;
+	arglist[1] = 0.001;
+	// gMinimizer->mnexcm("MIGRAD", arglist, 2, ierflag);
+	gMinimizer->mnexcm("SIMPLEX", arglist, 2, ierflag);
 
 	std::cout << "Showing fcn" << std::endl;
 	gMinimizer->mnexcm("SHO FCN", arglist, 2, ierflag);
