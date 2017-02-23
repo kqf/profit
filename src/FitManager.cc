02aaecdfbb893b850984fd73001f5e4432d03889
Коваленко Алексей Витальевич <petrov.knu@gmail.com>
2017-02-23 11:58:11 +0200

    Reenabled migrad. Still far from minimum.

--- src/FitManager.cc
+++ src/FitManager.cc
@@ -331,7 +331,7 @@ void FitManager::PerformMinimization(const char * outputfile)
 	// gMinimizer->mnexcm("MIGRAD", arglist, 2, ierflag);
 	gMinimizer->mnexcm("SIMPLEX", arglist, 2, ierflag);
 	arglist[0] = 1000;
-	// gMinimizer->mnexcm("MIGRAD", arglist, 2, ierflag);
+	gMinimizer->mnexcm("MIGRAD", arglist, 2, ierflag);
 
 	std::cout << "Showing fcn" << std::endl;
 	gMinimizer->mnexcm("SHO FCN", arglist, 2, ierflag);
