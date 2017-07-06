ada5ab3d450c74bcf8ae414a2427db65cf5acd92
Iohan Petrov <petrov.knu@gmail.com>
2017-07-06 22:40:48 +0200

    Fixed the performance plots script

--- test/plot-performance.py
+++ test/plot-performance.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 
 import numpy as np
 import seaborn
@@ -6,29 +6,32 @@ import matplotlib.pyplot as plt
 
 def read_data(filename):
     processors, times = np.loadtxt(filename).T
-    processors.astype(int)
+    processors = processors.astype(int)
 
     data = {}
     for p, t in zip(processors, times):
         if not p in data:
             data[p] = np.array([t])
 
-        data[p] = data[p] + [t]
+        data[p] = np.append(data[p], [t])
 
-
-    for p in data:
-        data[p] = data[p].mean()
-
-    return data.keys(), [data[p].mean() for p in data]
+    print data.keys()
+    print data
+    return data.keys(), [data[p].mean() for p in data], [data[p].std() for p in data]
 
 
 
 def main():
-    x, y = read_data('execution_time.txt')
 
-    plt.ylabel('execution time')
-    plt.xlabel('N processes')
-    plt.plot(x, y, 'o')
+    labels = ['intel', 'amd']
+    for label in labels:
+        x, y, err = read_data('execution_time.txt.{0}'.format(label))
+        print x, y
+        plt.ylabel('execution time, in seconds')
+        plt.xlabel('N processes')
+        plt.errorbar(x, y, yerr = err, fmt='o', label = label)
+        
+    plt.legend()
     plt.show()
 
 
